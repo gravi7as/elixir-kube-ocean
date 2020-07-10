5729c1061da92600b4c3c3b58ae694ec63f72705
gravitas <6ravitas@protonmail.com>
2020-07-09 21:23:41 -0700

    add tesla dep

--- mix.exs
+++ mix.exs
@@ -33,13 +33,15 @@ defmodule StarterService.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.3"},
+      {:gettext, "~> 0.11"},
+      {:hackney, "~> 1.15.2"},
+      {:jason, "~> 1.0"},
       {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix, "~> 1.5.3"},
+      {:plug_cowboy, "~> 2.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:tesla, "~> 1.3.0"}
     ]
   end
 
